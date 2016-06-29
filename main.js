62f004211e914683f0fb37bc2d75cb0cc1c6f889
Louie Alarcon <tumbando@gmail.com>
2016-06-29 15:06:05 -0500

    used filter to make array of items priced between  and

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-var prices = items.map(function(currentItem, index, array){
-  return currentItem.price;
+var prices = items.map(function(currentItem, index, array) {
+    return currentItem.price;
 });
-var priceSum = prices.reduce(function(a,b){
-  return a+b;
+
+var priceSum = prices.reduce(function(a, b) {
+    return a + b;
 });
-function priceAvg(prices){
-  return priceSum/prices.length;
+
+function priceAvg(prices) {
+    return priceSum / prices.length;
 }
+//--------------------------------------------------------------------
+var priceRange = items.filter(function(currentItem, index, array) {
+    if (currentItem.price >= 14.00 && currentItem.price <= 18.00) {
+        return currentItem.price;
+
+    }
+});
+//--------------------------------------------------------------------
