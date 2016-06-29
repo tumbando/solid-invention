d004c5afb46e55b30729f444ff519ddfdb9c1f2a
Louie Alarcon <tumbando@gmail.com>
2016-06-29 14:41:01 -0500

    made priceAvg into a function

--- main.js
+++ main.js
@@ -4,12 +4,6 @@ var prices = items.map(function(currentItem, index, array){
 var priceSum = prices.reduce(function(a,b){
   return a+b;
 });
-var priceAvg = (priceSum/prices.length);
-console.log(priceAvg);
-//priceSum divided by number of elements in priceSum array
-
-
-// var namesOnly = people.map(function(currentItem, index, array) {
-//   return currentItem.name;
-// });
-// console.log('5', namesOnly);
+function priceAvg(prices){
+  return priceSum/prices.length;
+}
